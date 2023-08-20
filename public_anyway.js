c93ff167e9f1173b4d567ba794bfe179623c249a
onedot6one8 <141647538+onedot6one8@users.noreply.github.com>
2023-08-20 19:29:45 +0200

    Update public_anyway.js

--- public_anyway.js
+++ public_anyway.js
@@ -1,3 +1,4 @@
 // env.js
-const alchemyApiKey = "13hmBcmjKMk3eroI0ntj1EXfJNEYV0lV";
+// const alchemyApiKey = "13hmBcmjKMk3eroI0ntj1EXfJNEYV0lV";
+const alchemyApiKey = "84R8PjloX6QoNDzhDy6bZ9tdoubnYRRx";
 export { alchemyApiKey };
